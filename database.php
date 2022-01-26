7067a2c1de100d256c732eb1fbdb88b51a1740d0
DmitriyTeterin <dmitriyteterin11@yandex.ru>
2022-01-26 21:12:00 +0700

    add function csvString

--- database.php
+++ database.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Переводит информацию из таблицы в массив.
+ * @param string $path
+ * @return array
+ */
 function csvArray ($path):array {
 
     if (($handle = fopen($path, "r")) !== FALSE) {
@@ -10,7 +14,7 @@ function csvArray ($path):array {
             $num = count($data);
             $item = [];
             
-            for ($c=0; $c < $num; $c++) {
+            for ($c = 0; $c < $num; $c++) {
                 if(is_numeric($data[$c]) === TRUE){
                    $item[$keys[$c]] = (int)$data[$c];
                }
@@ -23,8 +27,41 @@ function csvArray ($path):array {
     return $array;
 }
 
+/**
+ * Переводит информацию из таблицы в строку, далее из строки в массив.
+ * @param string $path
+ * @return array
+ */
+function csvString ($path):array{
+    $string = file_get_contents($path, true, null, 0);
+    $array = explode("\n", $string);
+    $keys []= $array[0];
+    $keys = explode(";", $keys[0]);
+    $num = count($array);
+    $cnt = count($keys);
+    
+    for($i = 1; $i < $num; $i++){
+        $horse = explode(";", $array[$i]);
+       
+        for($n = 0; $n < $cnt; $n++){
+           
+            if(is_numeric(trim($horse[$n])) === TRUE){
+                $horseArr[trim($keys[$n])] = (int)$horse[$n];
+                }
+            else{$horseArr[$keys[$n]] = $horse[$n];}       
+        }
+        $horses[$i-1] = $horseArr;
+    }
+    fclose;
+    return $horses;
+    
+}
+
+
+
+
 $tariffs = csvArray('./database/tariffs.csv');
-$cities = csvArray('./database/cities.csv');
-$colors = csvArray('./database/colors.csv');
-$horses = csvArray('./database/horses.csv');
-var_dump($tariffs);
+$cities = csvString('./database/cities.csv');
+$colors = csvString('./database/colors.csv');
+$horses = csvString('./database/horses.csv');
+
