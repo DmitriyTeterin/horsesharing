14b1a97c3480656830cc4653b61f88fbeca9198c
DmitriyTeterin <dmitriyteterin11@yandex.ru>
2022-03-03 01:25:08 +0700

    #16. Added a function that handles the user's request.

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-include 'database.php';
-include 'functionFilter.php';
-include 'functionFindByItem.php';
-include 'functionReceiveHorseInfo.php';
-
-const BASE_MULTIPLIER = 1;
-
-$horses = filter($horses, 'tariff', 2);
-$horses = filter($horses, 'city', 1);
-
-foreach ($horses as $horse) {
-    echo receiveHorseInfo($horse);
-}
+<?phpinclude 'functionRequest.php';const BASE_MULTIPLIER = 1;$getArray = $_GET;request($getArray);
