ed8bf1a4e39f00912b09f29336e28840a408cfd1
DmitriyTeterin <dmitriyteterin11@yandex.ru>
2022-03-03 01:20:24 +0700

    Revert "#16. Added a function that handles the user's request."
    This reverts commit 92c979bc6734278a8157ef26c381af0782ad8e1c.

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?phpinclude 'functionRequest.php';const BASE_MULTIPLIER = 1;$getArray = $_GET;request($getArray);
+<?php
+include 'database.php';
+include 'functionFilter.php';
+include 'functionFindByItem.php';
+include 'functionReceiveHorseInfo.php';
+
+const BASE_MULTIPLIER = 1;
+
+$horses = filter($horses, 'tariff', 2);
+$horses = filter($horses, 'city', 1);
+
+foreach ($horses as $horse) {
+    echo receiveHorseInfo($horse);
+}
