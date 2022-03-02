b0b646da9d74a5c512a5f35be3a065a145726db5
DmitriyTeterin <dmitriyteterin11@yandex.ru>
2022-03-03 01:20:11 +0700

    Revert "#16. Added a function that handles the user's request."
    This reverts commit 4daa9a3f449913f87ef07639ecf13c6b6ee0b3a3.

--- functionRequest.php
+++ functionRequest.php
@@ -1,1 +1,44 @@
-<?phpinclude 'database.php';include 'functionFilter.php';include 'functionFindByItem.php';include 'functionReceiveHorseInfo.php';/** * @return string * Проверяет есть ли GET запрос, если да то фильтрует по заданным параметрам и выводит на экран. */global $horses;function request(array $getArray){    global $horses;    if (empty($_GET) == true) {        foreach ($horses as $horse) {            echo receiveHorseInfo($horse);        }    } else {        $keysGetArray = array_keys($getArray);        $file = fopen('./database/horses.csv', "r");        $keysHorsesArray = fgetcsv($file, 1000, ";");        fclose($file);        $count = count($keysGetArray);        for ($i = 0; $i < $count; $i++) {            if (in_array($keysGetArray[$i], $keysHorsesArray)) {                $key = $keysGetArray[$i];                $value = $getArray[$key];                $value = is_numeric($value) == true ? (int)$value : $value;                $horses = filter($horses, $key, $value);            }        }        if (empty($horses) == false) {            foreach ($horses as $horse) {                echo receiveHorseInfo($horse);            }        } else {            echo "По вашему запросу лошадей не найдено.";        }    }}
+<?php
+include 'database.php';
+include 'functionFilter.php';
+include 'functionFindByItem.php';
+include 'functionReceiveHorseInfo.php';
+
+/**
+ * @return string
+ * Проверяет есть ли GET запрос, если да то фильтрует по заданным параметрам и выводит на экран.
+ */
+global $horses;
+
+function request(array $getArray)
+{
+    global $horses;
+    if (empty($_GET) == true) {
+        foreach ($horses as $horse) {
+            echo receiveHorseInfo($horse);
+        }
+    } else {
+        $keysGetArray = array_keys($getArray);
+        $file = fopen('./database/horses.csv', "r");
+        $keysHorsesArray = fgetcsv($file, 1000, ";");
+        fclose($file);
+        $count = count($keysGetArray);
+
+        for ($i = 0; $i < $count; $i++) {
+            if (in_array($keysGetArray[$i], $keysHorsesArray)) {
+                $key = $keysGetArray[$i];
+                $value = $getArray[$key];
+                $value = is_numeric($value) == true ? (int)$value : $value;
+                $horses = filter($horses, $key, $value);
+            }
+        }
+
+        if (empty($horses) == false) {
+            foreach ($horses as $horse) {
+                echo receiveHorseInfo($horse);
+            }
+        } else {
+            echo "По вашему запросу лошадей не найдено.";
+        }
+    }
+}
