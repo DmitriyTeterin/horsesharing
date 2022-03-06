2d99b1348af0ee28fa8b806d083f010ff01deea7
DmitriyTeterin <dmitriyteterin11@yandex.ru>
2022-03-06 23:01:35 +0700

    #16. Refactoring

--- functionRequest.php
+++ functionRequest.php
@@ -3,42 +3,71 @@ include 'database.php';
 include 'functionFilter.php';
 include 'functionFindByItem.php';
 include 'functionReceiveHorseInfo.php';
+/**
+ * Проверяет есть ли параметры в GET запросе.
+ * @param array $GET
+ * @return bool
+ */
+function checkEmptyRequest(array $GET): bool
+{
+    if (empty($GET)) {
+        $result = true;
+    } else $result = false;
+
+    return $result;
+}
 
 /**
- * @return string
- * Проверяет есть ли GET запрос, если да то фильтрует по заданным параметрам и выводит на экран.
+ * Ищет в GET запросе параметры схожие с параметрами из списка лошадей.
+ * @param $getArray
+ * @return array
  */
-global $horses;
+function checkingRequestParameters($getArray): array
+{
+    $file = fopen('./database/horses.csv', "r");
+    $keysHorsesArray = fgetcsv($file, 1000, ";");
+    fclose($file);
+    $count = count($keysHorsesArray);
+    $resultArray = [];
 
-function request(array $getArray)
+    for ($i = 0; $i < $count; $i++) {
+        $searchKey = $keysHorsesArray[$i];
+        $keysGetArray = array_key_exists($searchKey, $getArray);
+
+        if ($keysGetArray) {
+            $resultArray[$searchKey] = $getArray[$searchKey];
+        }
+    }
+    if (empty($resultArray)) {
+        echo 'Вы ввели несуществующие параметры фильтрации';
+    }
+    return $resultArray;
+}
+
+/**
+ * Фильтрует список лошадей по найденным в GET запросе параметрам.
+ * @param array $getArray
+ * @return void
+ */
+function filteringByGetRequest(array $getArray)
 {
     global $horses;
-    if (empty($_GET) == true) {
+    $count = count($getArray);
+    $keyArray = array_keys($getArray);
+
+    for ($i = 0; $i < $count; $i++) {
+        $key = $keyArray[$i];
+        $value = $getArray[$key];
+
+        $value = is_numeric($value) == true ? (int)$value : $value;
+        $horses = filter($horses, $key, $value);
+    }
+
+    if (empty($horses) == false) {
         foreach ($horses as $horse) {
             echo receiveHorseInfo($horse);
         }
     } else {
-        $keysGetArray = array_keys($getArray);
-        $file = fopen('./database/horses.csv', "r");
-        $keysHorsesArray = fgetcsv($file, 1000, ";");
-        fclose($file);
-        $count = count($keysGetArray);
-
-        for ($i = 0; $i < $count; $i++) {
-            if (in_array($keysGetArray[$i], $keysHorsesArray)) {
-                $key = $keysGetArray[$i];
-                $value = $getArray[$key];
-                $value = is_numeric($value) == true ? (int)$value : $value;
-                $horses = filter($horses, $key, $value);
-            }
-        }
-
-        if (empty($horses) == false) {
-            foreach ($horses as $horse) {
-                echo receiveHorseInfo($horse);
-            }
-        } else {
-            echo "По вашему запросу лошадей не найдено.";
-        }
+        echo "По вашему запросу лошадей не найдено.";
     }
 }
--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?phpinclude 'functionRequest.php';const BASE_MULTIPLIER = 1;$getArray = $_GET;request($getArray);
+<?php
+
+include 'functionRequest.php';
+
+const BASE_MULTIPLIER = 1;
+global $horses;
+
+$value = checkEmptyRequest($_GET);
+
+if ($value == true) {
+    foreach ($horses as $horse) {
+        echo receiveHorseInfo($horse);
+    }
+    die;
+}
+
+$getArray = checkingRequestParameters($_GET);
+
+if (empty($getArray)) {
+   die;}
+ else filteringByGetRequest($getArray);
